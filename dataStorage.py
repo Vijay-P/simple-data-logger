4c255e5078e50e2d4cb3b47a788f2bb52cd03453
Vijay Pillai <vijay.pillai397@gmail.com>
2018-03-19 16:34:14 -0500

    works

--- dataStorage.py
+++ dataStorage.py
@@ -4,12 +4,12 @@ import time
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-if(not os.path.isfile('../dataBase.csv')):
+if(not os.path.isfile('dataBase.csv')):
 	print("making file")
-	with open('../dataBase.csv', 'w') as f:
+	with open('dataBase.csv', 'w') as f:
 		f.write("time,light,temp\n")
-print(os.path.isfile('../dataBase.csv'))
+print(os.path.isfile('dataBase.csv'))
 while True:
-	with open('../dataBase.csv', 'a') as f:
+	with open('dataBase.csv', 'a') as f:
 		f.write(str(int(round(time.time()*1000)))+","+ser.readline())
 
